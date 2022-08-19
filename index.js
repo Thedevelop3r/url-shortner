6e82ef5dc5c78c9c74ebf3c0554805de102cfca2
Bilal-Arain <bilalarain444@gmail.com>
2022-08-19 22:56:25 +0500

    fix

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ connection.once("open", () => {
 app.use("/api/shorturl", route);
 
 // hosting homepage
-app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
-});
+// app.get("/", function (req, res) {
+//   res.sendFile(process.cwd() + "/views/index.html");
+// });
 // Your first API endpoint
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
